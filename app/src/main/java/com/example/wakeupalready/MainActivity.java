a6324ff375038c5d1e19c392938088ae72949cde
CrissSR <cristofitor1274@hotmail.com>
2023-10-23 23:55:50 -0500

    update: parte grafica Sign In completa falta conectar los iconos

--- app/src/main/java/com/example/wakeupalready/MainActivity.java
+++ app/src/main/java/com/example/wakeupalready/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.wakeupalready;
 
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correcto
+                    Toast.makeText(MainActivity.this, "Inicio de sesion Correcto",Toast.LENGTH_SHORT).show();
+                }else
+                    //error
+                    Toast.makeText(MainActivity.this, "Inicio de sesion Incorrecto",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
